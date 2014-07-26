d6f7ffacf4d83fde19e9823635bfc2a8f344f639
bellaincanto <shelagh_mcgowan@my.uri.edu>
2014-07-26 17:52:41 -0400

    troubleshooting

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Shelagh McGowan on 7/4/14.
 //  Copyright (c) 2014 Shelagh McGowan. All rights reserved.
 //
+//what did the baby computer call it's father? 
 
 import UIKit
 import CoreData
